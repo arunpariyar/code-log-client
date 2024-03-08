392e14f2a0a86c89e82470d99c86f8d03d94ace5
Arun Pariyar <arun.pariyar@gmail.com>
2024-03-08 16:10:42 +0100

    ✨ Add primary button, style change to feedback bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedbackBarComponent } from './components/feedback-bar/feedback-bar.component';
+import { PrimaryBtnComponent } from './components/buttons/primary-btn/primary-btn.component';
 
 @NgModule({
-  declarations: [AppComponent, FeedbackBarComponent],
+  declarations: [AppComponent, FeedbackBarComponent, PrimaryBtnComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/feedback-bar/feedback-bar.component.ts
+++ src/app/components/feedback-bar/feedback-bar.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-feedback-bar',
   templateUrl: './feedback-bar.component.html',
-  styleUrls: ['./feedback-bar.component.css']
+  styleUrls: ['./feedback-bar.component.css'],
 })
 export class FeedbackBarComponent {
-
+  noOfSgs = 6;
 }
