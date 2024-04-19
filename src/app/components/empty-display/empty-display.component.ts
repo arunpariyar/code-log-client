3790e73d8b8891d707fd297ba9c6a029ce101bc5
Arun Pariyar <arun.pariyar@gmail.com>
2024-04-19 10:29:35 +0200

    ✨ Add feedback submit form

--- src/app/components/empty-display/empty-display.component.ts
+++ src/app/components/empty-display/empty-display.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empty-display',
   templateUrl: './empty-display.component.html',
-  styleUrls: ['./empty-display.component.css']
+  styleUrls: ['./empty-display.component.css'],
 })
 export class EmptyDisplayComponent {
-
+  constructor(private router: Router) {}
+  navigateToForm() {
+    this.router.navigate(['/feedback-form']);
+  }
 }
--- src/app/components/new-feedback-form/new-feedback-form.component.ts
+++ src/app/components/new-feedback-form/new-feedback-form.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-feedback-form',
   templateUrl: './new-feedback-form.component.html',
-  styleUrls: ['./new-feedback-form.component.css']
+  styleUrls: ['./new-feedback-form.component.css'],
 })
 export class NewFeedbackFormComponent {
+  feedbackForm!: FormGroup;
+  categories = ['UI', 'UX', 'Enhancement', 'Bug', 'Feature'];
+  constructor(private router: Router, private formBuilders: FormBuilder) {
+    this.feedbackForm = this.formBuilders.group({
+      title: ['', Validators.required],
+      category: ['', Validators.required],
+      details: ['', Validators.required],
+    });
+  }
 
+  submitForm() {
+    if (this.feedbackForm.valid) {
+      console.log(this.feedbackForm.value);
+    }
+  }
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
 }
