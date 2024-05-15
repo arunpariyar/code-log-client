32e30ead0e0eef3024e185485880b4b3b0d1e4d2
Arun Pariyar <arun.pariyar@gmail.com>
2024-05-15 12:02:43 +0200

    ✅ Add tests for presentational component feedback

--- src/app/components/feedback/feedback.component.spec.ts
+++ src/app/components/feedback/feedback.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeedbackComponent } from './feedback.component';
+import { mockFeedbacks } from 'src/test-data/mock-feedbacks';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AppModule } from 'src/app/app.module';
 
-describe('FeedbackComponent', () => {
+fdescribe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
+  let el: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FeedbackComponent]
+      imports: [AppModule],
     });
+
     fixture = TestBed.createComponent(FeedbackComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render with values', () => {
+    component.feedback = mockFeedbacks[0];
+    fixture.detectChanges();
+    const container = el.queryAll(By.css('.feedback-container'));
+    expect(container.length).withContext('should only be one').toBe(1);
+    const title = el.query(By.css('.feedback-title'));
+    expect(title.nativeElement.textContent).toBe(mockFeedbacks[0].title);
+    const detail = el.query(By.css('.feedback-summary'));
+    expect(detail.nativeElement.textContent).toBe(mockFeedbacks[0].detail);
   });
 });
