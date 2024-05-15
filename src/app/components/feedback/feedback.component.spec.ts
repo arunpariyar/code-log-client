edea0c088ed8dd8d59f80f096867b84956ab7845
Arun Pariyar <arun.pariyar@gmail.com>
2024-05-15 13:21:55 +0200

    ✅ Add test setup
    have to comeback to testing again after as mock store will be required.

--- src/app/components/feedback/feedback.component.spec.ts
+++ src/app/components/feedback/feedback.component.spec.ts
@@ -5,7 +5,7 @@ import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { AppModule } from 'src/app/app.module';
 
-fdescribe('FeedbackComponent', () => {
+describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
   let el: DebugElement;
@@ -26,7 +26,9 @@ fdescribe('FeedbackComponent', () => {
 
   it('should render with values', () => {
     component.feedback = mockFeedbacks[0];
+    //this most come after as the feedback provided above
     fixture.detectChanges();
+
     const container = el.queryAll(By.css('.feedback-container'));
     expect(container.length).withContext('should only be one').toBe(1);
     const title = el.query(By.css('.feedback-title'));
